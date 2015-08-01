ff0fcc27820bebf2002469c569270297c415c6d2
Pea (MB) <pierrealix.tremblay@gmail.com>
2015-08-01 20:48:31 +0200

    update frequency

--- scheduler_hourly_mantras.js
+++ scheduler_hourly_mantras.js
@@ -4,5 +4,6 @@ Mantra = require('./mantras.js');
 
 var date = new Date();
 var hour = date.getHours();
-if (hour >= 8 && hour <= 22)
+console.log(hour);
+if (hour == 2 || hour == 8 || hour == 14 || hour == 20)
 	Nexmo.send("README", contacts.find_by_name("pa").phone, Mantra.hourly_mantras().join(' / '));
